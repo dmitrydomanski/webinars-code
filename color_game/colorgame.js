2fe9997228275b7a7104d164de546cb779dc6af3
Dominik Domanski <dominikdomanski@Dominiks-MacBook-Pro.local>
2019-01-27 08:49:15 +0200

    Buttons pending set-up

--- color_game/colorgame.js
+++ color_game/colorgame.js
@@ -1,21 +1,49 @@
-//5. Check if the clicked colour is a picked colour
-//6. Create win-loose behaviour
 function init(){
     const complexity = 9;
-    fillInSquares(complexity);
     const colorItems = document.querySelectorAll('.colorItem');
-    paintColorsUp(colorItems);
-    const pickedColour = pickColor(colorItems, complexity);
-    checkColor(colorItems, pickedColour);
+    setColours(complexity, colorItems);
+    setComplexityButton(complexity,colorItems);
+    document.querySelector('.header').style.background = 'forestgreen';
+    setResetButton(colorItems);
+    console.log(colorItems);
     const message = document.querySelector('#message');
 }
 
 init();
 
+function setColours(n){
+    fillInSquares(n);
+    const colorItems = document.querySelectorAll('.colorItem');
+    paintColorsUp(colorItems);
+    const pickedColour = pickColor(colorItems, n);
+    checkColor(colorItems, pickedColour);
+    
+}
+
+function setResetButton(array){
+    const reset = document.querySelector('#reset');
+    reset.addEventListener('click', () =>{
+        paintColorsUp(array);
+    })
+}
+
+function setComplexityButton(n, array){
+    const buttons = document.querySelectorAll('.mode');
+    buttons.forEach(element => {
+        element.addEventListener('click', ()=>{
+            if (element.textContent === 'Hard'){
+                complexity = 9;
+            } else {
+                complexity = 6;
+            }
+        });
+    });
+    setColours(n, array);
+}
 
 //1. Fill in squares
 function fillInSquares(num){
-    const wrapper = document.querySelector('#colorItemWrapper');
+    const wrapper = document.querySelector('#colorItems');
     for (i=0; i<num; i++){
         const square = document.createElement('div');
         square.classList.add('colorItem');
@@ -43,22 +71,37 @@ function paintColorsUp(array){
 function checkColor(array, pickedColour){
     array.forEach(element => {
         element.addEventListener('click', ()=>{
-           const color = element.style.background;
-           if (color === pickedColour) {
+            const color = element.style.background;
+            //5. Check if the clicked colour is a picked colour
+            if (color === pickedColour) {
+                //6. Create win-loose behaviour
+                message.style.color = 'white';
                 message.textContent = 'SUCCESS';
-           } else {
-            message.innerText = 'TRY AGAIN';
-           }
+                changeColors(pickedColour);
+            } else {
+                message.innerText = 'TRY AGAIN';
+                message.style.color = 'red';
+                element.style.background = 'black';
+            }
         })
     });
 }
 
 function pickColor(array, complexity){
     const num = Math.round(Math.random() * (complexity - 1));
+    // console.log(array);
     const color = array[num].style.background;
-    document.querySelector('#colorDisplay').textContent = color;
+    const colorString = `Try to click the square of color ${color}`
+    document.querySelector('#colorDisplay').textContent = colorString;
     return color;
 }
 
-
-// document.querySelector('.colorItem').style.background = generateRGB();
+// function changeColors(array, color){
+    function changeColors(color){
+    const colors = document.querySelectorAll('#colorItem');
+    // colors.forEach(element => {element.style.background = color});
+    for (i=0; i<colors.length; i++){
+        colors[i].style.background = color;
+    }
+    document.querySelector('.header').style.background = color;
+}
