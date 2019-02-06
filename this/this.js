2b43b4b4d37a91c4d64f555eb11b3cf34cefc997
Dominik Domanski <dominikdomanski@Dominiks-MacBook-Pro.local>
2019-02-06 09:15:51 +0200

    callback, promises, async/await

--- this/this.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    const print = () => console.log('Hi')
-    // $('button').click(print);
-    $('button').click(function(){
-        console.log('Hallo');
-    });
-});
