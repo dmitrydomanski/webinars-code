c718fb473baa13d9da8a19ee33106644d3ea3e77
Dominik Domanski <dominikdomanski@Dominiks-MacBook-Pro.local>
2019-02-21 22:17:43 +0200

    routing intro

--- node_intro/server.js
+++ node_intro/server.js
@@ -1,19 +1,27 @@
 const http = require('http');
+const url = require('url');
 
-const makeServer = ((request, response) => {
-    response.writeHead(200, {'Content-type': 'text/plain'});
-    response.write('Hi there');
-    response.end();
-})
+const makeServer = function (req, res) {
+    let path = url.parse(req.url).pathname;
+    console.log(path);
+    if (path === '/'){
+        res.writeHead(200, {'Content-type': 'text/plain'});
+        res.write('Basic path');
+    }
+    else if (path === '/about'){
+        res.writeHead(200, {'Content-type': 'text/plain'});
+        res.write('About page');
+    }
+    else if (path === '/blog'){
+        res.writeHead(200, {'Content-type': 'text/plain'});
+        res.write('Blog page');
+    }
+    else {
+        res.writeHead(404, {'Content-type': 'text/plain'});
+        res.write('Page not found');
+    }
+res.end();
+}
 
-server = http.createServer(makeServer);
-
-server.listen(3000, () => {
-    console.log('Node.js is working at port 3000');
-})
-
-    //   server.on('request', (request, response) => {
-    //       response.writeHead(200, {'Content-type': 'text/plain'});
-    //       response.write('Hello');
-    //       response.end();
-    //   });
+const server = http.createServer(makeServer);
+server.listen(3000, () => console.log('Server is up and listening'));
