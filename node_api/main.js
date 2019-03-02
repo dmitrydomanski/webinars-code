81c97e37b4be0f89f30636f10d8dc523a87e5eb4
Dominik Domanski <dominikdomanski@Dominiks-MacBook-Pro.local>
2019-03-02 18:49:55 +0200

    initial commit

--- node_api/main.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('button');
-btn.addEventListener('click', getData);
-
-const getData = () => {
-    console.log('getData');
-    fetch('/users')
-    .then(res => res.json())
-    .then(data => console.log(data));
-}
