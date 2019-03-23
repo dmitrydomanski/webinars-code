d0d7cbe5db418a31a342f6477369b1a647e4aa46
Dominik Domanski <dominikdomanski@Dominiks-MacBook-Pro.local>
2019-03-23 21:08:06 +0200

    mongoose introduction

--- blog-project/index.js
+++ blog-project/index.js
@@ -2,27 +2,47 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
-const articles = require('./articles');
 const bodyParser = require('body-parser');
+const Article = require('./models/article.model');
 
-
+//Middlewares
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css/'))
 
+// Set and configure template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//Mongoose connection
 mongoose.connect('mongodb://localhost/blogproject', { useNewUrlParser: true });
 let db = mongoose.connection;
 
 db.on('error', (err) => console.log(err));
 db.once('open', () => console.log('Mongo DB is connected'));
 
-app.get('/', (req, res) => res.render('index', {
-    title: 'Most viewed articles',
-    articles: articles
-}));
+//Routes
+app.get('/', (req, res) => {
+    // Article.find({}, (error, articles) => {
+    //     if (error) {
+    //         console.log(error);
+    //     } else {
+    //         res.render('index', {
+    //             title: 'Most viewed articles',
+    //             articles: articles
+    //         })
+    //     }
+    // })
+    try {
+        let articles = Article.find({});
+        res.render('index', {
+            title: 'Most viewed articles',
+            articles: articles
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 app.get('/articles/add', (req, res) => res.render('addArticle', {
     title: 'Add article'
@@ -31,15 +51,20 @@ app.get('/articles/add', (req, res) => res.render('addArticle', {
 app.post('/articles/add', (req, res) => {
     const { title, author, body } = req.body;
     const articleObject = {
-        id: articles.length + 1,
         title,
         author,
+        diplayText: body.slice(0, 140),
         body
     }
-    articles.unshift(articleObject);
-    res.redirect('/');
+    let article = new Article(articleObject);
+    article.save((error) => {
+        if (error) {
+            console.log(error);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    })
 });
-
-//ejs pug mustasche template engine
 
 app.listen(3000, () => console.log('server is up and running at port 3000'));
---
+++ blog-project/models/article.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+let articleSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    diplayText: {
+        type: String,
+        required: true,
+        default: 'Some text here'
+    },
+    body: {
+        type: String,
+        required: true
+    }
+});
+
+let article = module.exports = mongoose.model('Article', articleSchema );
