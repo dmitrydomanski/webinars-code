ead8ee9bf51ab4936a7caa5384a27832a5806812
Dominik Domanski <dominikdomanski@Dominiks-MacBook-Pro.local>
2019-04-13 20:09:27 +0300

    initial commit

--- blog-project/routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/articles', require('./articles'));
-router.use('/users', require('./users'));
-
-module.exports = router;
